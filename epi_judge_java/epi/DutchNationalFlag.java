399fb9bc398b42a915bec5132bb964820e33527c
Marcin.Charezinski <marcin.charezinski@foxsports.com.au>
2019-07-12 13:46:01 +1000

    Solution

--- epi_judge_java/epi/DutchNationalFlag.java
+++ epi_judge_java/epi/DutchNationalFlag.java
@@ -4,18 +4,43 @@ import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 public class DutchNationalFlag {
   public enum Color { RED, WHITE, BLUE }
 
   public static void dutchFlagPartition(int pivotIndex, List<Color> A) {
-    // TODO - you fill in here.
+//    [2,0,0,1,2,1]
+//    [1,0,0,1,2,2]
+
+
+    int start = 0;
+    int end = A.size()-1;
+    Color pivot = A.get(pivotIndex);
+
+    while(start < end) {
+
+      if (A.get(start) == pivot) {
+        while (A.get(end) != pivot) {
+          end--;
+        }
+        Collections.swap(A, start, end);
+        start++;
+      }
+
+      if (A.get(start) != pivot) {
+        Collections.swap(A, start, end);
+        start++;
+        end--;
+      }
+
+    }
     return;
   }
   @EpiTest(testDataFile = "dutch_national_flag.tsv")
   public static void dutchFlagPartitionWrapper(TimedExecutor executor,
                                                List<Integer> A, int pivotIdx)
-      throws Exception {
+          throws Exception {
     List<Color> colors = new ArrayList<>();
     int[] count = new int[3];
 
@@ -46,7 +71,7 @@ public class DutchNationalFlag {
 
     if (i != colors.size()) {
       throw new TestFailure("Not partitioned after " + Integer.toString(i) +
-                            "th element");
+              "th element");
     } else if (count[0] != 0 || count[1] != 0 || count[2] != 0) {
       throw new TestFailure("Some elements are missing from original array");
     }
