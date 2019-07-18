dbd1b2b7435a70937fec431caa2faadfa3891421
Marcin.Charezinski <marcin.charezinski@foxsports.com.au>
2019-07-19 08:52:55 +1000

    solved

--- epi_judge_java/epi/BuyAndSellStock.java
+++ epi_judge_java/epi/BuyAndSellStock.java
@@ -5,8 +5,14 @@ import java.util.List;
 public class BuyAndSellStock {
   @EpiTest(testDataFile = "buy_and_sell_stock.tsv")
   public static double computeMaxProfit(List<Double> prices) {
-    // TODO - you fill in here.
-    return 0.0;
+    double max = 0;
+    for(int i=0; i<prices.size(); i++){
+      for(int j=i+1; j<prices.size(); j++) {
+        max = Math.max(prices.get(j)- prices.get(i), max);
+      }
+
+    }
+    return max;
   }
 
   public static void main(String[] args) {
