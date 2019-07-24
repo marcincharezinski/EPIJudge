1262af72399bad2ce553e5b5bf8d819c1ae3e9b4
Marcin.Charezinski <marcin.charezinski@foxsports.com.au>
2019-07-25 09:52:33 +1000

    done

--- epi_judge_java/epi/AlternatingArray.java
+++ epi_judge_java/epi/AlternatingArray.java
@@ -1,73 +1,90 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 import epi.test_framework.TestUtils;
 import epi.test_framework.TimedExecutor;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
 public class AlternatingArray {
-  public static void rearrange(List<Integer> A) {
-    // TODO - you fill in here.
-    return;
-  }
-  private static void checkOrder(List<Integer> A) throws TestFailure {
-    for (int i = 0; i < A.size(); ++i) {
-      if ((i % 2) != 0) {
-        if (A.get(i) < A.get(i - 1)) {
-          throw new TestFailure()
-              .withProperty(TestFailure.PropertyName.RESULT, A)
-              .withMismatchInfo(
-                  i, String.format("A[%d] <= A[%d]", i - 1, i),
-                  String.format("%d > %d", A.get(i - 1), A.get(i)));
+    public static void rearrange(List<Integer> A) {
+        int i = 0;
+        while (i + 1 < A.size()) {
+            if (i % 2 == 0 && A.get(i) > A.get(i + 1)) {
+                Collections.swap(A, i, i + 1);
+                i++;
+            } else if (i % 2 != 0 && A.get(i) < A.get(i + 1)) {
+                Collections.swap(A, i, i + 1);
+                i++;
+            } else
+                i++;
         }
-        if (i < A.size() - 1) {
-          if (A.get(i) < A.get(i + 1)) {
-            throw new TestFailure()
-                .withProperty(TestFailure.PropertyName.RESULT, A)
-                .withMismatchInfo(
-                    i, String.format("A[%d] >= A[%d]", i, i + 1),
-                    String.format("%d < %d", A.get(i), A.get(i + 1)));
-          }
-        }
-      } else {
-        if (i > 0) {
-          if (A.get(i - 1) < A.get(i)) {
-            throw new TestFailure()
-                .withProperty(TestFailure.PropertyName.RESULT, A)
-                .withMismatchInfo(
-                    i, String.format("A[%d] >= A[%d]", i - 1, i),
-                    String.format("%d < %d", A.get(i - 1), A.get(i)));
-          }
-        }
-        if (i < A.size() - 1) {
-          if (A.get(i + 1) < A.get(i)) {
-            throw new TestFailure()
-                .withProperty(TestFailure.PropertyName.RESULT, A)
-                .withMismatchInfo(
-                    i, String.format("A[%d] <= A[%d]", i, i + 1),
-                    String.format("%d > %d", A.get(i), A.get(i + 1)));
-          }
-        }
-      }
+        return;
     }
-  }
 
-  @EpiTest(testDataFile = "alternating_array.tsv")
-  public static void rearrangeWrapper(TimedExecutor executor, List<Integer> A)
-      throws Exception {
-    List<Integer> result = new ArrayList<>(A);
-    executor.run(() -> rearrange(result));
 
-    TestUtils.assertAllValuesPresent(A, result);
-    checkOrder(result);
-  }
+    private static void checkOrder(List<Integer> A) throws TestFailure {
+        for (int i = 0; i < A.size(); ++i) {
+            if ((i % 2) != 0) {
+                if (A.get(i) < A.get(i - 1)) {
+                    throw new TestFailure()
+                            .withProperty(TestFailure.PropertyName.RESULT, A)
+                            .withMismatchInfo(
+                                    i, String.format("A[%d] <= A[%d]", i - 1, i),
+                                    String.format("%d > %d", A.get(i - 1), A.get(i)));
+                }
+                if (i < A.size() - 1) {
+                    if (A.get(i) < A.get(i + 1)) {
+                        throw new TestFailure()
+                                .withProperty(TestFailure.PropertyName.RESULT, A)
+                                .withMismatchInfo(
+                                        i, String.format("A[%d] >= A[%d]", i, i + 1),
+                                        String.format("%d < %d", A.get(i), A.get(i + 1)));
+                    }
+                }
+            } else {
+                if (i > 0) {
+                    if (A.get(i - 1) < A.get(i)) {
+                        throw new TestFailure()
+                                .withProperty(TestFailure.PropertyName.RESULT, A)
+                                .withMismatchInfo(
+                                        i, String.format("A[%d] >= A[%d]", i - 1, i),
+                                        String.format("%d < %d", A.get(i - 1), A.get(i)));
+                    }
+                }
+                if (i < A.size() - 1) {
+                    if (A.get(i + 1) < A.get(i)) {
+                        throw new TestFailure()
+                                .withProperty(TestFailure.PropertyName.RESULT, A)
+                                .withMismatchInfo(
+                                        i, String.format("A[%d] <= A[%d]", i, i + 1),
+                                        String.format("%d > %d", A.get(i), A.get(i + 1)));
+                    }
+                }
+            }
+        }
+    }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "AlternatingArray.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+    @EpiTest(testDataFile = "alternating_array.tsv")
+    public static void rearrangeWrapper(TimedExecutor executor, List<Integer> A)
+            throws Exception {
+        List<Integer> result = new ArrayList<>(A);
+        executor.run(() -> rearrange(result));
+
+        TestUtils.assertAllValuesPresent(A, result);
+        checkOrder(result);
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "AlternatingArray.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
