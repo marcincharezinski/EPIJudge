4fd55d57e9dd527efc4e7a4682c1d96c5bafcf73
Marcin.Charezinski <marcin.charezinski@foxsports.com.au>
2019-07-28 20:24:59 +1000

    tree

--- epi_judge_java/epi/IsTreeBalanced.java
+++ epi_judge_java/epi/IsTreeBalanced.java
@@ -1,20 +1,53 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class IsTreeBalanced {
 
-  @EpiTest(testDataFile = "is_tree_balanced.tsv")
+    @EpiTest(testDataFile = "is_tree_balanced.tsv")
+    public static boolean isBalanced(BinaryTreeNode<Integer> root) {
+        boolean result = true;
+        if (root != null) {
+            result = Math.abs(preOrderTraversal(root)-preOrderTraversalRight(root)) >= 2;
 
-  public static boolean isBalanced(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
-  }
+            isBalanced(root.left);
+            isBalanced(root.right);
+        }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IsTreeBalanced.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+        return result;
+    }
+
+
+    static void inOrderTraversal();
+
+    static int preOrderTraversal(BinaryTreeNode<Integer> root) {
+        int i = 0;
+        if (root != null) {
+            System.out.println(root.data);
+            i++;
+            preOrderTraversal(root.left);
+        }
+
+        return i;
+    }
+
+    static int preOrderTraversalRight(BinaryTreeNode<Integer> root) {
+        int i = 0;
+        if (root != null) {
+            System.out.println(root.data);
+            i++;
+            preOrderTraversal(root.right);
+        }
+        return i;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "IsTreeBalanced.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
--- epi_judge_java/epi/PrimeSieve.java
+++ epi_judge_java/epi/PrimeSieve.java
@@ -1,20 +1,46 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-import java.util.List;
-public class PrimeSieve {
-  @EpiTest(testDataFile = "prime_sieve.tsv")
-  // Given n, return all primes up to and including n.
-  public static List<Integer> generatePrimes(int n) {
-    // TODO - you fill in here.
-    return null;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "PrimeSieve.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeSieve {
+    @EpiTest(testDataFile = "prime_sieve.tsv")
+    // Given n, return all primes up to and including n.
+    public static List<Integer> generatePrimes(int n) {
+        List<Integer> result = new ArrayList<>();
+
+        if (Math.abs(n) < 2) {
+            return result;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                result.add(i);
+            }
+        }
+
+        return result;
+    }
+
+    static boolean isPrime(int n) {
+        for (int i = n - 1; i >= 2; i--) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "PrimeSieve.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
